6ef969cfdb988a5b25efc2f74b9ca034418727fb
Bruna Goncalves <brunagoncalves.sjb@gmail.com>
2024-10-14 17:15:01 -0300

    Adjustments to the project base

---
+++ src/data/configSite.ts
@@ -0,0 +1,10 @@
+export const configSite: ConfigSiteTypes = {
+  title: "AstroModel",
+  description: "Description WebSite",
+  author: "Author",
+  favicon: "favicon.ico",
+  imgLogo: "src/assets/images/logo.svg",
+  altLogo: "Logo AstroModel",
+  widthLogo: 144,
+  heightLogo: 37,
+};
---
+++ src/types/configSiteTypes.ts
@@ -0,0 +1,10 @@
+interface ConfigSiteTypes {
+  title: string;
+  description: string;
+  author: string;
+  favicon: string;
+  imgLogo: string;
+  altLogo: string;
+  widthLogo: number;
+  heightLogo: number;
+}
